348f31af1e2e53edb0ec9562be9a667d31289be5
neajul <info@lukasengelhardt.net>
2021-10-06 00:15:18 +0200

    sorting works except for blocks...

--- user/themes/homecinema2/js/tablesorting.js
+++ user/themes/homecinema2/js/tablesorting.js
@@ -1,7 +1,57 @@
 // on load
 $(function(){
   // add sorting function to table headers
-  $('.table-header li').not('.images').not('.diagram').click(function(){
+  $('.table-line.first li').not('.img').click(function(){
     changeActiveCategory($(this));
   });
 });
+
+// table ordering functions find active row
+function changeActiveCategory(item){
+  // first make sure we contain everything to this table
+  var table = item.parent().parent().parent();
+  // check if we are changing columns
+  if (!item.hasClass('selected')) {
+    // remove all selected classes
+    table.find('li').removeClass('selected').removeClass('asc').removeClass('desc');
+    // make this item selected
+    item.addClass('selected');
+  }
+  // check direction
+  if (item.hasClass('asc')) {
+    // if the item already is on ascending, change it to descending
+    item.removeClass('asc');
+    item.addClass('desc');
+  } else {
+    // otherwise always go ascending
+    item.removeClass('desc');
+    item.addClass('asc');
+  }
+  // get arguments: 0 is context, 1 is selected, 2 is direction
+  var arguments = item.attr("class").split(' ');
+  sortItems(table, arguments[0], arguments[2]);
+}
+
+
+// sort items in list alphabetically
+function sortItems(context, condition, direction){
+  var listitems = context.children('.table-line').not('.first').get();
+  listitems.sort(function(a, b) {
+    // default: desc
+    var item1 = a;
+    var item2 = b;
+    // if asc
+    if (direction == 'asc') {
+      var item1 = b;
+      var item2 = a;
+    }
+    // sort items, also take numbers into accounts as actual numbers
+    return $(item1).find('.' + condition + ' *').not('.block').eq(0).text().toUpperCase().localeCompare(
+      $(item2).find('.' + condition + ' *').not('.block').eq(0).text().toUpperCase(), undefined, {
+        numeric: true,
+        sensitivity: 'base'
+      }
+    );
+  })
+  $.each(listitems, function(idx, itm) { context.find('.table-line.first').not('.block').after(itm); });
+}
